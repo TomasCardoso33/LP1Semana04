0ae0d869b983b427e8a942f52facdf6195abe66f
Tomás <tomascardoso.dev@gmail.com>
2024-03-13 18:10:32 +0000

    Added for cycle to go trough the arguments

---
+++ BetterDecorator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BetterDecorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            char c = "";
+            int i = 0;
+
+            for (int i1 = 0; i1< args.length; i1++)
+            {
+                if ( i1 == 0)
+                    {
+                        s = args[i1];
+                    }
+                else if ( i1 == 1)
+                    {
+                        c = args[i1];
+                    }
+                else if ( i1 == 2)
+                    {
+                        i = args[i2];
+                    }
+            }
+        }
+    }
+}
