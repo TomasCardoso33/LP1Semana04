448b8bb048d3bff08ee341111d831a79a502403c
Tomás <tomascardoso.dev@gmail.com>
2024-03-14 22:55:12 +0000

    Added the methods with a cycle for

---
+++ Special/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Special
+{
+    
+
+    class Program
+    {
+        static int GetSpecial(int n)
+        {
+            int n1 = 1;
+            int n2 = 0;
+            int n3 = 0;
+            for ( int i = 2 ; i < n ; i ++ )
+            {
+                n2 = n2*2;
+                n3 = n1 + n2;
+                n1 = n3;
+
+            }
+            Console.WriteLine(n1);
+        }
+    
+        static void Main(string[] args)
+        {
+            GetSpecial(0);
+            GetSpecial(2);
+            GetSpecial(7);    
+        }
+         
+    }
+}
